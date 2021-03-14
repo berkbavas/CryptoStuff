7f0d0059f893e181298f3b82acc5e77ba6691e2b
berkbavas <berkbavas@gmail.com>
2021-03-14 23:46:15 +0300

    CBC padding oracle attack is implemented.

---
+++ src/main/java/attacks/cbc/Attacker.java
@@ -0,0 +1,85 @@
+package attacks.cbc;
+
+import java.util.Arrays;
+
+public class Attacker {
+    private CBCPaddingOracle oracle;
+
+    public Attacker(CBCPaddingOracle oracle) {
+        this.oracle = oracle;
+    }
+
+    public byte[] recover(byte[] ciphertext) {
+        if (ciphertext.length < 32) {
+            throw new IllegalArgumentException(String.format("Size of the ciphertext must be greater than or equal to %d.", 32));
+        }
+
+        if (ciphertext.length % 16 != 0) {
+            throw new IllegalArgumentException(String.format("Size of the ciphertext must be a multiple of %d.", 16));
+        }
+
+        byte[] plaintext = new byte[ciphertext.length - 16];
+        int blockCount = ciphertext.length / 16;
+        for (int i = 1; i < blockCount; i++) {
+            byte[] previousBlock = new byte[16];
+            byte[] currentBlock = new byte[16];
+            System.arraycopy(ciphertext, 16 * (i - 1), previousBlock, 0, 16);
+            System.arraycopy(ciphertext, 16 * i, currentBlock, 0, 16);
+            byte[] recoveredBlock = recoverBlock(previousBlock, currentBlock);
+            System.arraycopy(recoveredBlock, 0, plaintext, 16 * (i - 1), 16);
+        }
+
+        return Util.unpad(plaintext);
+    }
+
+    private byte[] recoverBlock(byte[] previousBlock, byte[] currentBlock) {
+        byte[] plaintextBlock = new byte[16];
+        if (oracle.query(previousBlock, currentBlock)) {
+            int index = 15;
+            for (int i = 0; i < 15; i++) {
+                ++previousBlock[i];
+                if (!oracle.query(previousBlock, currentBlock)) {
+                    --previousBlock[i];
+                    index = i;
+                    break;
+                }
+                --previousBlock[i];
+            }
+            Arrays.fill(plaintextBlock, index, 16, (byte) (16 - index));
+            byte[] newPreviousBlock = new byte[16];
+            --index;
+            for (int i = index; i >= 0; i--) {
+                System.arraycopy(previousBlock, 0, newPreviousBlock, 0, 16);
+                for (int j = 15; j > i; j--) {
+                    newPreviousBlock[j] = (byte) (plaintextBlock[j] ^ previousBlock[j] ^ (16 - i));
+                }
+                ++newPreviousBlock[i];
+                while (!oracle.query(newPreviousBlock, currentBlock)) {
+                    ++newPreviousBlock[i];
+                }
+                plaintextBlock[i] = (byte) (newPreviousBlock[i] ^ previousBlock[i] ^ (16 - i));
+            }
+
+        } else {
+            byte temp = previousBlock[15];
+            previousBlock[15]++;
+            while (!oracle.query(previousBlock, currentBlock)) {
+                previousBlock[15]++;
+            }
+            plaintextBlock = recoverBlock(previousBlock, currentBlock);
+            plaintextBlock[15] = (byte) (plaintextBlock[15] ^ previousBlock[15] ^ temp);
+
+        }
+        return plaintextBlock;
+    }
+
+    public CBCPaddingOracle getOracle() {
+        return oracle;
+    }
+
+    public void setOracle(CBCPaddingOracle oracle) {
+        this.oracle = oracle;
+    }
+
+
+}
---
+++ src/main/java/attacks/cbc/CBCPaddingOracle.java
@@ -0,0 +1,96 @@
+package attacks.cbc;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.SecureRandom;
+
+public class CBCPaddingOracle {
+    private int queryCount = 0;
+    private SecretKey key;
+
+    public CBCPaddingOracle() {
+        reset();
+    }
+
+    public boolean query(byte[] a, byte[] b) {
+        return query(Util.concat(a, b));
+    }
+
+    public boolean query(byte[] queryArray) {
+        try {
+
+            byte[] plaintext = decrypt(queryArray);
+
+        } catch (Exception e) {
+            if (e instanceof BadPaddingException) {
+                queryCount++;
+                return false;
+            }
+            e.printStackTrace();
+        }
+
+        queryCount++;
+        return true;
+    }
+
+    public byte[] decrypt(byte[] encrypted) throws Exception {
+        if (encrypted.length < 32) {
+            throw new IllegalArgumentException(String.format("Size of the input array must be greater than or equal to %d.", 32));
+        }
+
+        if (encrypted.length % 16 != 0) {
+            throw new IllegalArgumentException(String.format("Size of the input array must be a multiple of %d.", 16));
+        }
+
+        // encrypted = iv + ciphertext
+        byte[] iv = new byte[16];
+        System.arraycopy(encrypted, 0, iv, 0, iv.length);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+
+        byte[] ciphertext = new byte[encrypted.length - iv.length];
+        System.arraycopy(encrypted, iv.length, ciphertext, 0, ciphertext.length);
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key, ivParameterSpec);
+        byte[] plaintext = cipher.doFinal(ciphertext);
+
+        return plaintext;
+    }
+
+    public byte[] encrypt(byte[] plaintext) throws Exception {
+        byte[] iv = new byte[16];
+        SecureRandom sr = new SecureRandom();
+        sr.nextBytes(iv);
+
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key, ivParameterSpec);
+        byte[] ciphertext = cipher.doFinal(plaintext);
+
+        // result = iv + ciphertext
+        byte[] result = new byte[iv.length + ciphertext.length];
+        System.arraycopy(iv, 0, result, 0, iv.length);
+        System.arraycopy(ciphertext, 0, result, iv.length, ciphertext.length);
+
+        return result;
+    }
+
+    public int getQueryCount() {
+        return queryCount;
+    }
+
+    public void reset() {
+        byte[] bytes = new byte[16];
+        SecureRandom sr = new SecureRandom();
+        sr.nextBytes(bytes);
+        key = new SecretKeySpec(bytes, "AES");
+        queryCount = 0;
+    }
+
+    public SecretKey getKey() {
+        return key;
+    }
+}
